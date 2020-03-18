57685980328dba925f8b1c3fd928177cb756bdc8
davidliudawei <mathprogliu@163.com>
2020-03-19 00:06:56 +0800

    dddd

--- unity.bat
+++ unity.bat
@@ -2,7 +2,7 @@
 
 echo 正在生成APK文件...  
 
-REM D:\Program Files\Unity2018.3.5\Editor\Unity.exe -projectPath E:\pig\TestGitInit\318 -quit -batchmode -executeMethod APKBuild.Build -logFile build.log
+REM D:\Program Files\Unity2018.3.5\Editor\Unity.exe -projectPath D:\UnityCreate\320 -quit -batchmode -executeMethod APKBuild.Build -logFile build.log
 
 %1 -projectPath %2 -quit -batchmode -executeMethod APKBuild.Build -logFile build.log
 
