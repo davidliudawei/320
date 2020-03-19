1d2837085d22bebb46fc1dbc4bc00af84a594f28
davidliudawei <mathprogliu@163.com>
2020-03-19 22:50:30 +0800

    fefdfe

--- unity2.bat
+++ unity2.bat
@@ -3,7 +3,7 @@
 echo 正在生成APK文件...  
 
 
-${D:\Program Files\Unity2018.3.5\Editor\Unity.exe} -batchmode -projectPath ${D:\UnityCreate\test} -executeMethod UwaProjScan.MainScan.DoTest -logFile
+D:\Program Files\Unity2018.3.5\Editor\Unity.exe -batchmode -projectPath D:\UnityCreate\test -executeMethod UwaProjScan.MainScan.DoTest -logFile
 echo APK文件生成完毕!  
 pause 
 
