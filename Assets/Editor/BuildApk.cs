a9388a50e1da9ec422d730636204ebbff9ef6198
davidliudawei <mathprogliu@163.com>
2020-03-19 22:13:23 +0800

    ddeefe

--- Assets/Editor/BuildApk.cs
+++ Assets/Editor/BuildApk.cs
@@ -12,15 +12,15 @@ public class BuildApk : Editor
         // 切换到 Android 平台
         EditorUserBuildSettings.SwitchActiveBuildTarget(buildTarget);
 
-        //// keystore 路径, G:\keystore\one.keystore
-        PlayerSettings.Android.keystoreName = "F:\\keystore\\user.keystore";
-        //// one.keystore 密码
-        PlayerSettings.Android.keystorePass = "liu120423";
+        ////// keystore 路径, G:\keystore\one.keystore
+        //PlayerSettings.Android.keystoreName = "F:\\keystore\\user.keystore";
+        ////// one.keystore 密码
+        //PlayerSettings.Android.keystorePass = "liu120423";
 
-        //// one.keystore 别名
-        PlayerSettings.Android.keyaliasName = "user";
-        //// 别名密码
-        PlayerSettings.Android.keyaliasPass = "liu120423";
+        ////// one.keystore 别名
+        //PlayerSettings.Android.keyaliasName = "user";
+        ////// 别名密码
+        //PlayerSettings.Android.keyaliasPass = "liu120423";
 
         List<string> levels = new List<string>();
         foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
@@ -31,7 +31,7 @@ public class BuildApk : Editor
         }
 
         // 打包出 APK 名
-        string apkName = string.Format("./{0}.apk", "Test");
+        string apkName = string.Format("./{0}.apk", "woshishui");
         // 执行打包
         var res = BuildPipeline.BuildPlayer(levels.ToArray(), apkName, buildTarget, BuildOptions.None);
        
