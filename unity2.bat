b2ed97ecc7ac9fd8d6495a56389db097e96ddeba
davidliudawei <mathprogliu@163.com>
2020-03-19 22:47:49 +0800

    dfef

--- unity2.bat
+++ unity2.bat
@@ -4,7 +4,6 @@ echo 正在生成APK文件...
 
 
 ${D:\Program Files\Unity2018.3.5\Editor\Unity.exe} -batchmode -projectPath ${D:\UnityCreate\test} -executeMethod UwaProjScan.MainScan.DoTest -logFile
-${LogFile} -quit
 echo APK文件生成完毕!  
 pause 
 
